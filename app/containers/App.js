b97bdded60a0f4907584621b7bd98520b23bcacc
Angel Grablev <agrublev@gmail.com>
2018-09-25 12:30:30 -0700

    INITIAL APP

---
+++ app/containers/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class App extends Component<Props> {
+    constructor (props) {
+        super(props);
+        console.log('BVRAND NEWt');
+
+        window.addEventListener('hashchange', function (event) {
+            console.log(event);
+        });
+    }
+
+    handleChange (e) {
+        console.log(e);
+
+    }
+
+    componentDidUpdate () {
+        console.log('BVRAND UPPPP');
+    }
+
+    render () {
+        console.log('BVRAND NEWt RENDER');
+        return (
+            <div id="wrap">
+                <input type="text"
+                    placeholder="start typing for commands..."
+                    ref={(ref) => this.ref}
+                    onKeyUp={(e) => this.handleChange(e)}
+                />
+            </div>
+        );
+    }
+}
---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
+import App from './containers/App'
+import './app.global.css'
+
+render(
+    <AppContainer>
+        <App/>
+    </AppContainer>,
+    document.getElementById('root')
+)
+
+if (module.hot) {
+    module.hot.accept('./containers/App', () => {
+        const NextRoot = require('./containers/App') // eslint-disable-line
+        // global-require
+        render(
+            <AppContainer>
+                <NextRoot/>
+            </AppContainer>,
+            document.getElementById('root')
+        )
+    })
+}
