9eb44d221b71fd2bca59446cbd9d3728dabd5d69
Angel Grablev <agrublev@gmail.com>
2018-09-25 12:22:11 -0700

    Bash

--- app/utils/customInput.js
+++
@@ -1,9 +0,0 @@
-const capture = require('../utils/capture/input')
-
-function selectItem (items, callback) {
-  capture.prompt(items).then((answer) => {
-    callback(answer)
-  })
-}
-
-module.exports = selectItem
